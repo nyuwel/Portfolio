d57a2aae7c8fb377ca85ac9f5f68779989f908de
Noel Tuyay <tuyaynoel30@gmail.com>
2026-05-27 19:43:30 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function sendMail() {
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
-    }
-    emailjs.send("service_rdb1bu1", "template_d2k630k", parms).then(alert("Message Sent!"))          
-}
